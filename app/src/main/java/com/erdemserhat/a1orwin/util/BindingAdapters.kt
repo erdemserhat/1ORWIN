6952ed6df044c7320e1e10b86bcebccb43f8d6d4
Serhat <me.serhaterdem@gmail.com>
2024-01-19 19:31:47 +0300

    hardware operations added..

--- app/src/main/java/com/erdemserhat/a1orwin/util/BindingAdapters.kt
+++ app/src/main/java/com/erdemserhat/a1orwin/util/BindingAdapters.kt
@@ -1,5 +1,10 @@
 package com.erdemserhat.a1orwin.util
 
+import android.animation.ValueAnimator
+import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.BindingAdapter
@@ -17,6 +22,44 @@ fun setDiceDrawable(view: View, diceNumber:Int){
         else -> R.drawable.dice_number_1
     }
     val drawable = ResourcesCompat.getDrawable(view.resources, drawableId, null)
+    animateAlpha(view)
     view.background = drawable
 
+    if(diceNumber==1){
+        vibrate(view,100)
+    }
 }
+
+
+private fun vibrate(view: View, millisecondsValue:Long){
+    val v = (view.context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        v.vibrate(
+            VibrationEffect.createOneShot(millisecondsValue,
+                VibrationEffect.DEFAULT_AMPLITUDE))
+    }
+    else {
+        v.vibrate(millisecondsValue)
+    }
+}
+private fun animateAlpha(view:View) {
+    // Create a ValueAnimator that animates the alpha value from 0 to 1
+    val animator = ValueAnimator.ofFloat(0f, 1f)
+
+    // Add an update listener to the animator to receive callbacks when the animation value changes
+    animator.addUpdateListener { animation ->
+        // Get the current animated alpha value
+        val alphaValue = animation.animatedValue as Float
+
+        // Set the alpha value of the diceView
+        view.alpha = alphaValue
+    }
+
+    // Set the duration of the animation in milliseconds
+    animator.duration = 500
+
+    // Start the animation
+    animator.start()
+}
+
